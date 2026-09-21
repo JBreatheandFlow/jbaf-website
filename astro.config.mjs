979ac9004616e4a37ca53ae51e64c4c1731dcec7
JBreatheandFlow <info@jessbreatheandflow.com>
2026-09-21 13:33:29 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jbreatheandflow.github.io',
-  base: '/jbaf-website',
+  site: 'https://jessbreatheandflow.com',
+  base: '/',
   trailingSlash: 'ignore',
 });
